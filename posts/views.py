ad9520e115e01de5b1ab05422b68aa2b3151f756
rohitatzignuts <rohitv@zignuts.com>
2024-10-07 14:25:05 +0530

    postslist and post page

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Post
+
 
 # Create your views here.
+def postsList(request):
+    posts = Post.objects.all().order_by("-date")
+    return render(request, "posts/list.html", {"posts": posts})
+
+
+def postPost(request, slug):
+    post = Post.objects.get(slug=slug)
+    return render(request, "posts/post.html", {"post": post})
